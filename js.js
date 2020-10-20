4b0d27fe954282c6608a545bf57606e77f724d1e
simmytun <71146368+simmytun@users.noreply.github.com>
2020-10-20 03:47:37 -0600

    Add files via upload

---
+++ js.js
@@ -0,0 +1,22 @@
+const lightning = document.querySelector('.lightning');
+const button = document.querySelector('button');
+
+button.addEventListener('click', function(){
+
+lightning.classList.toggle('spark');
+
+
+const bread = document.querySelector('.bread');
+const button = document.querySelector('button');
+
+button.addEventListener('click', function(){
+
+bread.classList.toggle('roast');
+  
+});
+
+  
+});
+
+
+
